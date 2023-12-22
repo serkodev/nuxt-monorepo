30d1000d304ec8bb8c8e858b5a0d4d68246c27b6
SerKo <serko.dev@gmail.com>
2023-12-22 20:18:18 +0800

    feat: import components

--- packages/app/app.vue
+++ packages/app/app.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
+import type { UiTheMessage } from '#components'
+
+const theMessage = ref<InstanceType<typeof UiTheMessage> | null>()
+
+onMounted(() => {
+  console.log('theMessage', theMessage.value)
+})
+
 const config = useRuntimeConfig()
 </script>
 
 <template>
   <div>
     {{ config.public.apiBase }}
-    <UiTheMessage />
+    <UiTheMessage ref="theMessage" />
   </div>
 </template>
